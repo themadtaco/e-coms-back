61e5d78bd6640f289f0da9fa1277d4cc75a96ad6
themadtaco <aatrox48@gmail.com>
2022-03-05 19:42:59 -0600

    added associations for models

--- models/index.js
+++ models/index.js
@@ -5,12 +5,31 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+  onDelete: 'SET NULL'
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  as: 'tags',
+  foreignKey: 'product_id',
+  onDelete: 'SET NULL'
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  as: 'tags',
+  foreignKey: 'tag_id',
+  onDelete: 'SET NULL'
+});
 
 module.exports = {
   Product,
